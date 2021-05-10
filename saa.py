6efd1b7eb54a19b930dcb74921f0c658dfe9cf94
opastorello <68306729+opastorello@users.noreply.github.com>
2021-05-09 22:50:57 -0300

    Update saa.py

--- saa.py
+++ saa.py
@@ -26,7 +26,7 @@ class SeleniumAliAutomation:
 			user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 			profile = webdriver.FirefoxProfile() 
 			profile.set_preference("general.useragent.override", user_agent)
-			profile.executable_path = r'./driver/geckodriver.exe'
+			profile.executable_path = r'./geckodriver.exe'
 
 			self.browser = webdriver.Firefox(profile, options=firefox_options)
 			self.browser.set_window_size(360,640)
